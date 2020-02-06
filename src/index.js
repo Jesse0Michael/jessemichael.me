dc082362315abc473b0263146b5995db350ea459
Jesse Michael <jesse.michael@faithlife.com>
2020-02-06 12:48:42 -0700

    Analytics in react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-25334252-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
